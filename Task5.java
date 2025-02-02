de74ca993ec6d6d3f572ba0373a5bdb8da83d4a1
Ali Usama <aliusama.king@gmail.com>
2025-02-02 13:40:06 +0500

    Update Task5.java

--- Task5.java
+++ Task5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Task5 {
+class Task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int rows = 4, cols = 4;
